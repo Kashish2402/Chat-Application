ec67fb96d51178855671b524cfbcd7d56f50aa1c
Kashish2402 <Kashishgupta2402@gmail.com>
2025-02-11 19:16:25 +0530

    added message model

--- src/models/messages.models.js
+++ src/models/messages.models.js
@@ -1,17 +1,23 @@
 import mongoose, { Schema } from "mongoose";
 
-const messageSchema=new Schema({
-    senderId:{
-        type:String,
-        required:true
+const messageSchema = new Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
     },
-    recieverId:{
-        type:String,
-        required:true
+    recieverId: {
+      type: String,
+      required: true,
     },
-    content:{
-        type:String,
-        required:true
+    content: {
+      type: String,
+      required: true,
     },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},{timestamps:true})
+export const Message = mongoose.model("Message", messageSchema);
